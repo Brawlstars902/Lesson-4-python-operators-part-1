6ab52ef19eef1612134d156c8f7e17c494151425
Brawlstars902 <aparna.rautela@gmail.com>
2025-07-15 09:27:27 +0530

    Add files via upload

---
+++ Calculating averages.py
@@ -0,0 +1,11 @@
+a=11
+b=22
+c=33
+d=44
+e=55
+print ('These are the numbers',a,',',b,',',c,',',d,'and',e,'.')
+f=a+b+c+d+e
+print ('The sum of the numbers is',f,'.')
+g=f/5
+g=int(g)
+print ('The average of all the numbers is',g,'.')
